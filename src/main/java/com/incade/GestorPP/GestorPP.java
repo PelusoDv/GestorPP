708249c00874b2ec3e7ddea4a6479432ebc2a0d0
soporteNahuelSalinas <soporte.anyway02@gmail.com>
2025-06-17 00:50:26 -0300

    Dale!

--- src/main/java/com/incade/GestorPP/GestorPP.java
+++ src/main/java/com/incade/GestorPP/GestorPP.java
@@ -1,4 +1,4 @@
-package com.incade.GestorPP;
+package com.incade.gestorpp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

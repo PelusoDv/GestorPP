3c819b954bf63daf030e63fdef77a17373cab582
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-06-22 18:20:12 -0300

    No hace falta el MovimientosReoisitorio

--- src/main/java/com/incade/GestorPP/Repositorio/MovimientoRepositorio.java
+++
@@ -1,9 +0,0 @@
-
-package com.incade.GestorPP.Repositorio;
-
-import com.incade.GestorPP.Entidad.Movimiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimientoRepositorio extends JpaRepository<Movimiento, Integer> {
-
-}

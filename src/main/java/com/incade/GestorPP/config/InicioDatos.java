532f974402e9a9cdd59d4e50c111982ad0d747f4
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-06-23 09:06:44 -0300

    Añadi la categoria "Otro" para Ingresos y Gastos

--- src/main/java/com/incade/GestorPP/config/InicioDatos.java
+++ src/main/java/com/incade/GestorPP/config/InicioDatos.java
@@ -13,21 +13,20 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class InicioDatos implements CommandLineRunner {
-
     private final CategoriaGastoRepositorio repoCatG;
     private final CategoriaIngresoRepositorio repoCatI;
 
     @Override
     public void run(String... args) {
         if (repoCatG.count() == 0) {
-            List<String> categoriasGasto = List.of("Comida", "Transporte", "Servicios");
+            List<String> categoriasGasto = List.of("Comida", "Transporte", "Servicio", "Otro");
             categoriasGasto.forEach(nombre ->
                 repoCatG.save(new CategoriaGasto(null, nombre))
             );
         }
 
         if (repoCatI.count() == 0) {
-            List<String> categoriasIngreso = List.of("Salario", "Premio", "Venta");
+            List<String> categoriasIngreso = List.of("Salario", "Premio", "Venta", "Otro");
             categoriasIngreso.forEach(nombre ->
                 repoCatI.save(new CategoriaIngreso(null, nombre))
             );

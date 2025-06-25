4d22e63108c1a8009405cd410a3c328f329494e6
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-06-24 22:09:06 -0300

    Modificado el WebConfig para trabajar el fron en remoto

--- src/main/java/com/incade/GestorPP/config/WebConfig.java
+++ src/main/java/com/incade/GestorPP/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:3000")  // tu frontend React
+      .allowedOrigins("https://known-moccasin-magical.ngrok-free.app/")  // Frontend React
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+      .allowedHeaders("*")
       .allowCredentials(true);  // permitir cookies para sesión
   }
 }

5b207f4df0971258823acd2425e0e0f958efdfd4
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-06-29 19:14:21 -0300

    Añadida la nueva url de Vercel

--- src/main/java/com/incade/GestorPP/config/WebConfig.java
+++ src/main/java/com/incade/GestorPP/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("https://known-moccasin-magical.ngrok-free.app/")  // Frontend React
+      .allowedOrigins("https://known-moccasin-magical.ngrok-free.app/","https://fintrack-pearl.vercel.app/" )  // Frontend React
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);  // permitir cookies para sesión

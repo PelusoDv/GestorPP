51569a51b4b705c86a40c9e0679594fcfd1faef8
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-09-23 15:40:00 -0300

    intentando con Docker en render

--- src/main/java/com/incade/GestorPP/Controlador/MovimientoControlador.java
+++ src/main/java/com/incade/GestorPP/Controlador/MovimientoControlador.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -63,7 +64,7 @@ public class MovimientoControlador {
     }
     
     @GetMapping("/categorias")
-    public List<String> categorias(String tipo) {
+    public List<String> categorias(@RequestParam String tipo) {
         return service.listarCategorias(tipo);
     }
     
